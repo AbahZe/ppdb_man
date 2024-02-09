21557a0d2bf9509c5f4dca82524e9d93356f5012
AbahZe <orangdesa1989@gmail.com>
2024-02-10 01:33:53 +0800

    first commit

---
+++ application/config/nenemo.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['sitename'] = 'PPDB <br>MAN Model 1 Manado';
+$config['sitename_mini'] = '<i class="fab fa-gg"></i>';
+$config['title'] = 'PPDB';
+$config['botton'] = 'btn btn-success btn-flat';
+$config['header'] = 'bg-blue';
+// ====> mohon jangan di ganti
+$config['tahun'] = '2024';
+$config['copyright'] = 'MAN 1 Model Manado';
+// ====> dev
+$config['developer'] = 'Hairul Azmi';
+$config['email'] = 'ilung82@gmail.com';
+$config['contact'] = '082184032134';
+$config['workplace'] = 'SMP Negeri 1 Tumijajar';
+$config['project'] = 'PPDB';
+$config['codename'] = 'Manado';
+$config['version'] = '2.5.0';
+$config['license'] = '...';
